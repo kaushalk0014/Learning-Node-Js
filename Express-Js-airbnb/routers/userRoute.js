0d70842e5bd469173cb56a68079700fa62cb53f3
kaushalk0014 <kaushal.k.0014@gmail.com>
2025-10-29 20:25:07 +0530

    addd path module

--- Express-Js-airbnb/routers/userRoute.js
+++ Express-Js-airbnb/routers/userRoute.js
@@ -1,11 +1,14 @@
+
+//Code Module
+const path = require("path");
+
+//External Modules
 const express = require("express");
 
 const userRouter = express.Router();
 
 userRouter.get("/", (req, res, next) => {
-  res.send(`<h1>Welcome airbnb</h1>
-      <a href="/host/add-home">Add Home</a>
-    `);
+  res.sendFile(path.join(__dirname, "../", "views",  "home.html"));
 });
 
 module.exports = userRouter;
